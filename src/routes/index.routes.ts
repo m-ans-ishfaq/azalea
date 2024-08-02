e2caa109c40fa1aee35a97b350e1ff853fbb07b0
Ans <anasashfaq00@gmail.com>
2024-08-02 19:36:22 +0500

    Added basic routes

---
+++ src/routes/index.routes.ts
@@ -0,0 +1,10 @@
+import { Application } from "express";
+import users from "./user.routes";
+import auth from "./auth.routes";
+
+export const configRoutes = (app: Application): void => {
+    
+    app.use('/users', users);
+    app.use('/auth', auth);
+    
+}
