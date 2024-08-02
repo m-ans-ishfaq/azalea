d368d76b7658058f3d00068a7d8ef2554c55da24
Ans <anasashfaq00@gmail.com>
2024-08-02 19:35:28 +0500

    Setup Logger

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
+import 'module-alias/register';
 import express from 'express';
-import { configMiddleware } from './middlewares';
+import { configMiddleware } from '@/middlewares';
+import { connectToDatabase } from '@/utils/db-connection';
+import { config as envConfig } from 'dotenv';
+import { configRoutes } from './routes/index.routes';
+import { logger } from './utils/logger';
+
+envConfig();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 configMiddleware(app);
+configRoutes(app);
 
 app.get('/', (req, res) => {
     res.send('Hello Azalea!');
 });
 
+connectToDatabase();
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    logger.info(`Server is running on port ${PORT}`);
 });
 
 export { app };
